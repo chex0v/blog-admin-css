0a53a2dedd74b1df12a4a485e413497dadbe8e92
chexov <chex0ff@inbox.ru>
2022-06-12 16:10:52 +0300

    feature added admin pagination Added detail page for blog

---
+++ src/blocks/modules/detail-page/detail-page.js
@@ -0,0 +1,13 @@
+modules.define('detail-page', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
